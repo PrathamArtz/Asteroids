b1a2b8265482b27ce1f92470fb0c8dbbfdf1946f
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-18 23:38:15 +0530

    shooting direction issue solved

--- Asteroids/Assets/Scripts/Bullet.cs
+++ Asteroids/Assets/Scripts/Bullet.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bullet : MonoBehaviour
 {
+    private Player player;
     public float speed = 500f;
     public float maxtime = 10.0f;
     private Rigidbody2D rb;
@@ -11,12 +13,15 @@ public class Bullet : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
-    public void Projectile(Vector2 direction)
+    public void Projectile(Vector2 target)
     {
-        rb.AddForce(direction * speed);
-
+        /* rb.AddForce(direction * speed);
+         Destroy(gameObject, maxtime);*/
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        rb.velocity = direction * speed; // Directly setting velocity for accurate direction
         Destroy(gameObject, maxtime);
     }
 
