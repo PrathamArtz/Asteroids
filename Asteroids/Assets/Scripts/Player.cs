fa406639e3c4da6c8afd86ca50c67c61efa9a33d
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-18 18:46:51 +0530

    bullet shoot

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     private float playerSpeed = 3;
     private Rigidbody2D rb;
     private Camera cam;
@@ -16,7 +17,7 @@ public class Player : MonoBehaviour
         cam = GameObject.Find("Camera").GetComponent<Camera>();
     }
 
-    
+
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
@@ -24,12 +25,22 @@ public class Player : MonoBehaviour
         rb.transform.Translate(Vector2.up * verticalInput);
 
         mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
-    }
 
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
     private void FixedUpdate()
     {
         Vector2 lookDir = mousepos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(bulletPrefab, transform.position,transform.rotation);
+        bullet.Projectile(mousepos);
     }
 }
