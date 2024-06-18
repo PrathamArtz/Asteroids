7aabc03fe1f7e45ffae4780599b05113c542c7f3
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-18 17:29:13 +0530

    rotate player on mouse position

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float playerSpeed = 5;
+    private float playerSpeed = 3;
     private Rigidbody2D rb;
+    private Camera cam;
+
+    Vector2 mousepos;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
     }
 
     
@@ -18,5 +22,14 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
 
         rb.transform.Translate(Vector2.up * verticalInput);
+
+        mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 lookDir = mousepos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
