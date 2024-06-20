ebc6d1cf87f1152aae46103cf5e7e3d2b0a01fc2
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-20 22:30:58 +0530

    Game Over Ui added

--- Asteroids/Assets/Scripts/GameManager.cs
+++ Asteroids/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,6 +8,8 @@ public class GameManager : MonoBehaviour
     private Player player;
     public int lives = 3;
     public float respawnTime = 3f;
+    public TextMeshProUGUI GameOver_Text;
+    public TextMeshProUGUI pressed_Restart;
 
 
     private void Start()
@@ -43,6 +46,20 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
+        GameOver_Text.gameObject.SetActive(true);
+        pressed_Restart.gameObject.SetActive(true);
+        StartCoroutine(PressedToStart());
         Debug.Log("Game Over");
+    }
+
+    IEnumerator PressedToStart()
+    {
+        while(true)
+        { 
+            pressed_Restart.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            pressed_Restart.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
